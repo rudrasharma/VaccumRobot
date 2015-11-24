4e944636f872af9b3d28c47900c6e6e22fa77394
rudrasharma <rudra.sharma@gmail.com>
2015-11-23 22:06:20 -0500

    remove source code and move dependency to maven

--- src/com/googlecode/lanterna/gui2/dialogs/TextInputDialogResultValidator.java
+++
@@ -1,8 +0,0 @@
-package com.googlecode.lanterna.gui2.dialogs;
-
-/**
- * Created by martin on 23/06/15.
- */
-public interface TextInputDialogResultValidator {
-    String validate(String content);
-}
